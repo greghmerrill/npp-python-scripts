cb279da63b6f7eb6ae8cd936a55a62c5cfd567ac
Greg Merrill <greghmerrill@gmail.com>
2011-10-04 18:24:54 -0500

    new subproc utility

--- scriptlib.py
+++ scriptlib.py
@@ -1,4 +1,4 @@
-import re
+import re, subprocess
 from Npp import *
 
 def remove_blank_lines(txt):
@@ -14,3 +14,17 @@ def lines_to_sql_set(txt, xform=lambda txt: txt):
 def get_current_view_files():
   current_view = notepad.getCurrentView()
   return [file for file in notepad.getFiles() if file[3] == current_view]
+
+# Similar to subprocess.Popen, but automatically return a string containing
+# stdout and stderr content, and raise an Exception if the process results in
+# a non-zero return code
+def subproc(cmd_line):
+  # Prevent windows from creating a console window for this command
+  startupinfo = subprocess.STARTUPINFO()
+  startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+  process = subprocess.Popen(cmd_line, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, startupinfo=startupinfo)
+  out = process.communicate()[0]
+  if process.returncode != 0:
+    console.writeError(out)
+    raise Exception('p4 command failed: ' + out)
+  return out
